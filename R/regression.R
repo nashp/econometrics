47d2d24831dcd0f525a8db951ddc7a9102035305
nashp <nashp@tcd.ie>
2020-10-16 16:36:26 +0100

    latest update

--- R/regression.R
+++ R/regression.R
@@ -1,5 +1,10 @@
 library(readxl)
 library(dplyr)
+library(ggplot2)
 le_data <- read_excel("../data/LifeExpGDP17.xlsx")
   
-le_data <- le_data %>% dplyr::
+le_data <- le_data %>% mutate(LoPCGDP = log(PCGDP)) 
+
+reg <- lm(LE ~ LoPCGDP, le_data)
+summary(reg)
+anova(reg)
