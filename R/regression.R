ddbb69341a6948c786f673879299bf5e2919377c
Peter Nash <peter.nash@mbam.local>
2020-10-16 17:46:14 +0100

    calcs for Beta, errors, variances

--- R/regression.R
+++ R/regression.R
@@ -8,3 +8,27 @@ le_data <- le_data %>% mutate(LoPCGDP = log(PCGDP))
 reg <- lm(LE ~ LoPCGDP, le_data)
 summary(reg)
 anova(reg)
+
+# y = XB + u
+
+#Matrix form where Beta_hat = (X'X)^-1 X' y
+
+X <- as.matrix(le_data$LoPCGDP, nrow=nrow(le_data))
+y <- as.matrix(le_data$LE, nrow=nrow(le_data))
+rep(1, nrow(X))
+X <- cbind(rep(1, nrow(X)), X)
+T <- nrow(X)
+k <- ncol(X)
+#Errors 
+
+u <- y - X%*%beta_hat
+
+
+# Now Var(B) which is = sigma^2 (X' X) ^ -1
+# we estimate sigma^2 by u'u/T-k
+
+sigma2_hat <- t(u) %*% u /(T-k)
+sigma2_hat <- sigma2_hat[,1] 
+var_Beta_hat <- sigma2_hat * solve((t(X) %*% X))
+
+std_error <- sqrt(diag(var_Beta_hat))
