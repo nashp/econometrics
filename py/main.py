a6f308f5530c12ceff9841026cee4131779fe039
nashp <nashp@tcd.ie>
2020-10-18 21:39:12 +0100

    python regression

--- py/main.py
+++ py/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import math
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 filename = "../data/LifeExpGDP17.xlsx"
 
 le_data = pd.read_excel(filename)
-
 le_data["LoPCGDP"] = le_data["PCGDP"].transform(math.log)
 
 # Non Linear Relationshop
@@ -13,6 +13,20 @@ le_data.plot.scatter(x='PCGDP', y='LE')
 
 # Linear relationship
 le_data.plot.scatter(x='LoPCGDP', y='LE')
+X = np.array(le_data.LoPCGDP).reshape(-1, 1)
+y = np.array(le_data.LE)
 
-reg = LinearRegression().fit(X=le_data.LoPCGDP, y=le_data.LE)
+reg = LinearRegression().fit(X=X, y=y)
+
+X = np.concatenate([np.ones(X.shape), X], axis=1)
+X_t = np.transpose(X)
+# (X'X)^-1X'y
+
+beta_hat = np.linalg.inv(np.matmul(X_t, X))
+beta_hat = np.matmul(beta_hat, X_t)
+beta_hat = np.matmul(beta_hat, y)
+
+
+
+
 
