04bb261483963809ddbe06e620c9d90b1fe33f10
nashp <nashp@tcd.ie>
2020-11-27 17:00:57 +0000

    latest updates

--- R/arima.r
+++ R/arima.r
@@ -1,11 +1,11 @@
 # ARDL/ECM Lecture 6B
-
 library(readxl)
 library(dplyr)
 library(ggplot2)
 library(roll)
 library(car)
 library(xts)
+library(tseries)
 sh_data <- read_excel("../data/Shiller16.xlsx")
 
 # Because ND & NE have very notiecable trends we can 
@@ -22,12 +22,18 @@ sh_data <- sh_data %>%
                 trend =  row_number(), 
                 DeltaD = LD - LDlag,
                 DeltaE = LE - LElag, 
-                DeltalNSP = lNSP - dplyr::lag(lNSP)) %>%
+                DeltalNSP = lNSP - dplyr::lag(lNSP),
+                lPE = log(NSP/NE)) %>%
   filter(is.finite(NE), is.finite(ND))
 
 st = xts(sh_data$lNSP, order.by = as.Date.yearmon(sh_data$YEAR))
+pe <-xts(sh_data$lPE, order.by = as.Date.yearmon(sh_data$YEAR))
 
 adf.test(sh_data$lNSP, k=0)
 adf.model <- lm(DeltalNSP ~ trend + lNSPlag, data=sh_data)
 
 ar = arima(st, order=c(1, 0, 0), method="ML")
+
+
+
+
